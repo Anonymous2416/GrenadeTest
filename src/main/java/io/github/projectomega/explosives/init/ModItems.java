e1ad7b2f9a24e888e09bdba1eae4ff23eb62fd61
joey9 <cultureneverwins@gmail.com>
2024-05-30 13:32:07 -0400

    initial

---
+++ src/main/java/io/github/projectomega/explosives/init/ModItems.java
@@ -0,0 +1,18 @@
+package io.github.projectomega.explosives.init;
+
+import io.github.projectomega.explosives.Explosives;
+import io.github.projectomega.explosives.item.MK2GrenadeItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+                        Explosives.MOD_ID);
+
+        // TODO: Blanace numbers / adjust constructors as needed for special grenades
+        public static final RegistryObject<Item> MK2_GRENADE = ITEMS.register("grenade",
+                        () -> new MK2GrenadeItem(new Item.Properties(), 20 * 4, 5f, 1.3f));
+
+}
